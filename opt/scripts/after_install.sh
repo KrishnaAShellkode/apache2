cae977ea582bc53936009dcff3c93a1d3311d6af
KrishnaAShellkode <166110689+KrishnaAShellkode@users.noreply.github.com>
2024-04-12 14:05:56 +0530

    Update after_install.sh

--- opt/scripts/after_install.sh
+++ opt/scripts/after_install.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
+# Disable def.conf site
+a2dissite /etc/apache2/sites-available/def.conf
+systemctl reload apache2
+
+# Get the current working directory
+cwd=$(pwd)
+
+# Change to the deployment root directory
+cd /opt/codedeploy-agent/deployment-root/
+
+# Find all .conf files in the current directory and its subdirectories
+conf_files=$(find . -name "*.conf")
+
+# Iterate over the .conf files and enable them
+for conf_file in $conf_files; do
+   a2ensite "$conf_file"
+done
+
+# Reload Apache to apply changes
+systemctl reload apache2
+
+# Change back to the original working directory
+cd "$cwd"
+
+# Restart Apache
 sudo service apache2 restart
